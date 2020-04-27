c5602c27a2a2464e2f8b2d88e92645f2a742caa7
Lucas Morais <lucas.moraissz07@gmail.com>
2020-04-27 12:35:03 -0300

    add: add option to stop with maximum number of cycles or maximum error

--- project5/project_5.py
+++ project5/project_5.py
@@ -15,7 +15,7 @@ def run_neural_network():
         stop_type = 0,
         max_error = 0.05,
         learning_rate = 0.01,
-        max_cycles= 0,
+        max_cycles= 10,
         actual_cycle = 0,
     )
 
@@ -44,36 +44,68 @@ def run_neural_network():
     vetor1 = []
     vetor2 = []
 
-    
-    while(actual_error > neural_net.max_error ):
-        actual_error = 0
-        for i in range(samples_size):
-            #Calculate pure output
+    if neural_net.stop_type == 0:
 
-            yin = calculate_pure_output(i,loaded_txt_entries,classes_size,entries_size,v,v0)
-            
-            #Calculate liquid innput
-            y = calculate_output(classes_size,threshold,yin)
+        while(actual_error > neural_net.max_error ):
+            actual_error = 0
+            for i in range(samples_size):
+                #Calculate pure output
+
+                yin = calculate_pure_output(i,loaded_txt_entries,classes_size,entries_size,v,v0)
+                
+                #Calculate liquid innput
+                y = calculate_output(classes_size,threshold,yin)
 
 
-            #calculate new error
-            
-            actual_error = atualize_error(classes_size,actual_error,loaded_txt_targets,i,y)
-            
-            # Atualizing syntactic weights
-            v = atualize_syntactic_weights(v,entries_size,classes_size,neural_net.learning_rate,loaded_txt_targets,loaded_txt_entries,i,y)
+                #calculate new error
+                
+                actual_error = atualize_error(classes_size,actual_error,loaded_txt_targets,i,y)
+                
+                # Atualizing syntactic weights
+                v = atualize_syntactic_weights(v,entries_size,classes_size,neural_net.learning_rate,loaded_txt_targets,loaded_txt_entries,i,y)
 
-            #Atualizing bias
-            v0 = atualize_bias(v0,classes_size,loaded_txt_targets,neural_net.learning_rate,y,i)
+                #Atualizing bias
+                v0 = atualize_bias(v0,classes_size,loaded_txt_targets,neural_net.learning_rate,y,i)
 
-            
-            
-       
-        vetor1.append(actual_cycle)
-        vetor2.append(actual_error)
-        actual_cycle += 1
-    #END TRAINING
-    
+                
+                
+        
+            vetor1.append(actual_cycle)
+            vetor2.append(actual_error)
+            actual_cycle += 1
+        #END TRAINING
+
+
+    if neural_net.stop_type == 1:
+        
+        while(actual_cycle < neural_net.max_cycles ):
+            actual_error = 0
+            for i in range(samples_size):
+                #Calculate pure output
+
+                yin = calculate_pure_output(i,loaded_txt_entries,classes_size,entries_size,v,v0)
+                
+                #Calculate liquid innput
+                y = calculate_output(classes_size,threshold,yin)
+
+
+                #calculate new error
+                
+                actual_error = atualize_error(classes_size,actual_error,loaded_txt_targets,i,y)
+                
+                # Atualizing syntactic weights
+                v = atualize_syntactic_weights(v,entries_size,classes_size,neural_net.learning_rate,loaded_txt_targets,loaded_txt_entries,i,y)
+
+                #Atualizing bias
+                v0 = atualize_bias(v0,classes_size,loaded_txt_targets,neural_net.learning_rate,y,i)
+
+                
+                
+        
+            vetor1.append(actual_cycle)
+            vetor2.append(actual_error)
+            actual_cycle += 1
+        #END TRAINING
 
     show_chart2(vetor2,vetor1)
     plt.scatter(vetor1,vetor2,color='blue')
