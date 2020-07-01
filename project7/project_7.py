3f3baedf10cb5b3fdf83ca26e94afdfd2cb8c378
wendler <ramoswendler@gmail.com>
2020-07-01 19:30:47 -0300

    Creating project 7 files

---
+++ project7/project_7.py
@@ -0,0 +1,199 @@
+import numpy as np
+import random as rd
+import os
+import matplotlib.pyplot as plt
+
+
+def run_neural_network_7():
+    os.chdir(r"project7/digitos/digitostreinamento")
+    # Inicializando variaveis
+    ampdigitos = 40
+    vsai = 10
+    amostras = ampdigitos * vsai
+    entradas = 256
+    neur = 200
+    limiar = 0.0
+    alfa = 0.005
+    errotolerado = 0.1
+    listaciclo = []
+    listaerro = []
+
+    # Montando o arquivo de amostras de treinamento
+    x = np.zeros((amostras, entradas))
+    k2 = "_"
+    k4 = ".txt"
+    cont = 0
+    ordem = np.zeros(amostras)
+
+    for m in range(vsai):
+        k1 = str(m)
+
+        for n in range(ampdigitos):
+            k3a = n + 1
+            k3 = str(k3a)
+            nome = k1 + k2 + k3 + k4
+            entrada = np.loadtxt(nome)
+            x[cont, :] = entrada[:]
+            ordem[cont] = m
+            cont = cont + 1
+
+    ordem = ordem.astype("int")
+
+    # Lendo o arquivo de saídas esperadas
+    t = np.loadtxt("target10.csv", delimiter=";", skiprows=0)
+
+    # Gerando os pesos sinapticos aleatoriamente
+    vanterior = np.zeros((entradas, neur))
+    aleatorio = 0.2
+    for i in range(entradas):
+        for j in range(neur):
+            vanterior[i][j] = rd.uniform(-aleatorio, aleatorio)
+
+    v0anterior = np.zeros((1, neur))
+
+    for j in range(neur):
+        v0anterior[0][j] = rd.uniform(-aleatorio, aleatorio)
+
+    wanterior = np.zeros((neur, vsai))
+    aleatorio = 0.2
+    for i in range(neur):
+        for j in range(vsai):
+            wanterior[i][j] = rd.uniform(-aleatorio, aleatorio)
+
+    w0anterior = np.zeros((1, vsai))
+    for j in range(vsai):
+        w0anterior[0][j] = rd.uniform(-aleatorio, aleatorio)
+
+    # Matrizes de atualização de pesos e valores de saida da rede
+    vnovo = np.zeros((entradas, neur))
+    v0novo = np.zeros((1, neur))
+    wnovo = np.zeros((neur, vsai))
+    w0novo = np.zeros((1, vsai))
+    zin = np.zeros((1, neur))
+    z = np.zeros((1, neur))
+    deltinhak = np.zeros((vsai, 1))
+    deltaw0 = np.zeros((vsai, 1))
+    deltinha = np.zeros((1, neur))
+    xaux = np.zeros((1, entradas))
+    h = np.zeros((vsai, 1))
+    target = np.zeros((vsai, 1))
+    deltinha2 = np.zeros((neur, 1))
+    ciclo = 0
+    errototal = 100000
+
+    # Começo do treinamento da rede
+
+    while errotolerado < errototal:
+        errototal = 0
+        for padrao in range(amostras):
+            for j in range(neur):
+                zin[0][j] = np.dot(x[padrao, :], vanterior[:, j]) + v0anterior[0][j]
+
+            z = np.tanh(zin)
+            yin = np.dot(z, wanterior) + w0anterior
+            y = np.tanh(yin)
+
+            for m in range(vsai):
+                h[m][0] = y[0][m]
+            for m in range(vsai):
+                target[m][0] = t[m][ordem[padrao]]
+
+            errototal = errototal + np.sum(0.5 * ((target - h) ** 2))
+
+            # obter matrizes para correções dos pesos
+
+            deltinhak = (target - h) * (1 + h) * (1 - h)
+            deltaw = alfa * (np.dot(deltinhak, z))
+            deltaw0 = alfa * deltinhak
+            deltinhain = np.dot(np.transpose(deltinhak), np.transpose(wanterior))
+            deltinha = deltinhain * (1 + z) * (1 - z)
+
+            for m in range(neur):
+                deltinha2[m][0] = deltinha[0][m]
+            for k in range(entradas):
+                xaux[0][k] = x[padrao][k]
+            deltav = alfa * np.dot(deltinha2, xaux)
+            deltav0 = alfa * deltinha
+
+            # Realizando as atualizações de pesos
+            vnovo = vanterior + np.transpose(deltav)
+            v0novo = v0anterior + np.transpose(deltav0)
+            wnovo = wanterior + np.transpose(deltaw)
+            w0novo = w0anterior + np.transpose(deltaw0)
+            vanterior = vnovo
+            v0anterior = v0novo
+            wanterior = wnovo
+            w0anterior = w0novo
+        ciclo = ciclo + 1
+        listaciclo.append(ciclo)
+        listaerro.append(errototal)
+        print("Ciclo e Erro")
+        print(ciclo, errototal)
+
+    plt.plot(listaciclo, listaerro)
+    plt.xlabel("Ciclo")
+    plt.ylabel("Erro")
+    plt.show()
+    ###Teste automático da rede
+    aminicial = 20
+    amtestedigitos = 30
+    yteste = np.zeros((vsai, 1))
+    cont = 0
+    contcerto = 0
+    ordem = np.zeros(amostras)
+    for m in range(10):
+        k1 = str(m)
+        for n in range(amtestedigitos):
+            k3a = n + aminicial
+            k3 = str(k3a)
+            nome = k1 + k2 + k3 + k4
+            xteste = np.loadtxt(nome)
+            print(nome)
+
+            for m2 in range(vsai):
+                for n2 in range(neur):
+                    zin[0][n2] = np.dot(xteste, vanterior[:, n2]) + v0anterior[0][n2]
+                z = np.tanh(zin)
+                yin = np.dot(z, wanterior) + w0anterior
+                y = np.tanh(yin)
+
+        for j in range(vsai):
+            if y[0][j] >= limiar:
+                y[0][j] = 1.0
+            else:
+                y[0][j] = -1.0
+
+        for j in range(vsai):
+            yteste[j][0] = y[0][j]
+
+        for j in range(vsai):
+            target[j][0] = t[0][j]
+
+        soma = np.sum(y - target)
+
+        if soma == 0:
+            contcerto = contcerto + 1
+
+        cont = cont + 1
+
+    taxa = contcerto / cont
+    print("A taxa é: ")
+    print(taxa)
+    np.savetxt(
+        "vanterior_taxa_" + str(taxa).replace(".", "") + ".txt",
+        vanterior,
+        delimiter=";",
+    )
+    np.savetxt(
+        "wanterior_" + str(taxa).replace(".", "") + ".txt", wanterior, delimiter=";"
+    )
+    np.savetxt(
+        "v0nanterior_taxa_" + str(taxa).replace(".", "") + ".txt",
+        v0anterior,
+        delimiter=";",
+    )
+    np.savetxt(
+        "w0nanterior_taxa_" + str(taxa).replace(".", "") + ".txt",
+        w0anterior,
+        delimiter=";",
+    )
--- run.py
+++ run.py
@@ -3,9 +3,11 @@ from project2.project_2 import run_genetic_algoritm_1, test
 from project1.project_1 import run_genetic_algoritm
 from project5.project_5 import run_neural_network
 from project6.project_6 import run_neural_network2
+from project7.project_7 import run_neural_network_7
 
 #  Algoritm_option = input("Select the project number: ")
-algoritm_option = 6
+
+algoritm_option = 7
 if int(algoritm_option) == 1:
     run_genetic_algoritm()
 elif int(algoritm_option) == 2:
@@ -19,3 +21,6 @@ elif int(algoritm_option) == 5:
     run_neural_network()
 elif int(algoritm_option) == 6:
     run_neural_network2()
+
+elif int(algoritm_option) == 7:
+    run_neural_network_7()
